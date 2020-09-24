29f3057ace663f30ce7e4e48e26bd61ec806e2ce
asalaerekat <asala.erekat@mssm.edu>
2020-09-24 09:21:09 -0400

    Create Trial_branch.R

---
+++ Trial_branch.R
@@ -0,0 +1,25 @@
+# Get today and yesterday's date
+
+Today <- as.timeDate(format(Sys.Date(),"%m/%d/%Y"))
+#Today <- as.timeDate(as.Date("07/29/2020", format = "%m/%d/%Y"))
+
+
+#Determine if yesterday was a holiday/weekend 
+#get yesterday's DOW
+Yesterday <- as.timeDate(format(Sys.Date()-1,"%m/%d/%Y"))
+#Yesterday <- as.timeDate(as.Date("07/28/2020", format = "%m/%d/%Y"))
+
+#Get yesterday's DOW
+Yesterday_Day <- dayOfWeek(Yesterday)
+
+#Remove Good Friday from MSHS Holidays
+NYSE_Holidays <- as.Date(holidayNYSE(year = 1990:2100))
+GoodFriday <- as.Date(GoodFriday())
+MSHS_Holiday <- NYSE_Holidays[GoodFriday != NYSE_Holidays]
+
+#Determine whether yesterday was a holiday/weekend
+Holiday_Det <- isHoliday(Yesterday, holidays = MSHS_Holiday)
+
+#Set up a default calendar for collect to received TAT calculations for Pathology and Cytology
+create.calendar("MSHS_working_days", MSHS_Holiday, weekdays=c("saturday","sunday"))
+bizdays.options$set(default.calendar="MSHS_working_days")
